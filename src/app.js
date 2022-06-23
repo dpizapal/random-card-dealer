5bacae16a5fc0deb116107ab6b0ad834a067b3a5
dpizapal <dpizapal@yahoo.es>
2022-06-23 13:26:40 +0000

    boton

--- src/app.js
+++ src/app.js
@@ -54,3 +54,54 @@ window.onload = function() {
     newBottomSuit.style.color = "red";
   }
 };
+
+// Funcionalidad botón
+//
+//
+document.querySelector("#btn").addEventListener("click", () => {
+  //función cambiar número
+  let randomNumber = () => {
+    let numbers = [
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K",
+      "A"
+    ];
+    let chosenNumber = Math.floor(Math.random() * numbers.length);
+    return numbers[chosenNumber];
+  };
+  //función cambiar pinta
+  let randomSuit = () => {
+    let suits = ["&spades;", "&hearts;", "&diams;", "&clubs;"];
+    let chosenSuit = Math.floor(Math.random() * suits.length);
+
+    return suits[chosenSuit];
+  };
+  //cambiar número
+  let newNumber = document.querySelector("p");
+  newNumber.innerHTML = randomNumber();
+  //cambiar pinta
+  let newSuit = randomSuit();
+
+  let newTopSuit = document.querySelector(".suit-top");
+  newTopSuit.innerHTML = newSuit;
+
+  let newBottomSuit = document.querySelector(".suit-bottom");
+  newBottomSuit.innerHTML = newSuit;
+
+  //cambiar color
+
+  if (newSuit == "&hearts;" || newSuit == "&diams;") {
+    newTopSuit.style.color = "red";
+    newBottomSuit.style.color = "red";
+  }
+});
