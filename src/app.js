3c1fd3cdf223b11d0a3c1529d7e1e8642bab8824
dpizapal <dpizapal@yahoo.es>
2022-06-25 14:18:55 +0000

    sin cuenta atras

--- src/app.js
+++ src/app.js
@@ -5,39 +5,7 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-
-  //función cambiar número
-  let randomNumber = () => {
-    let chosenNumber = Math.floor(Math.random() * numbers.length);
-    return numbers[chosenNumber];
-  };
-  //función cambiar pinta
-  let randomSuit = () => {
-    let chosenSuit = Math.floor(Math.random() * suits.length);
-    return suits[chosenSuit];
-  };
-  //cambiar número
-  let newNumber = document.querySelector("p");
-  newNumber.innerHTML = randomNumber();
-  //cambiar pinta
-  let newSuit = randomSuit();
-
-  let newTopSuit = document.querySelector(".suit-top");
-  newTopSuit.innerHTML = newSuit;
-
-  let newBottomSuit = document.querySelector(".suit-bottom");
-  newBottomSuit.innerHTML = newSuit;
-
-  //cambiar color
-
-  if (newSuit == "&hearts;" || newSuit == "&diams;") {
-    newTopSuit.style.color = "red";
-    newBottomSuit.style.color = "red";
-  }
-};
-// Numeros y Picas
+//variables numeros y pinta
 let numbers = [
   "2",
   "3",
@@ -54,38 +22,37 @@ let numbers = [
   "A"
 ];
 let suits = ["&spades;", "&hearts;", "&diams;", "&clubs;"];
-
-// Funcionalidad botón
-//
-//
-document.querySelector("#btn").addEventListener("click", () => {
-  //función cambiar número
-  let randomNumber = () => {
-    let chosenNumber = Math.floor(Math.random() * numbers.length);
-    return numbers[chosenNumber];
-  };
-
-  //función cambiar pinta
-  let randomSuit = () => {
-    let chosenSuit = Math.floor(Math.random() * suits.length);
-    return suits[chosenSuit];
-  };
-
-  //cambiar número
-  let newNumber = document.querySelector("p");
-  newNumber.innerHTML = randomNumber();
-  //cambiar pinta
+//funcion numero aleatorio
+const randomNumber = () => {
+  let chosenNumber = Math.floor(Math.random() * numbers.length);
+  return numbers[chosenNumber];
+};
+//funcion pinta aleatoria
+const randomSuit = () => {
+  let chosenSuit = Math.floor(Math.random() * suits.length);
+  return suits[chosenSuit];
+};
+//función onload pagina
+window.onload = function cardRandom() {
   let newSuit = randomSuit();
+
+  //picas y numeros
   let newTopSuit = document.querySelector(".suit-top");
   newTopSuit.innerHTML = newSuit;
 
   let newBottomSuit = document.querySelector(".suit-bottom");
   newBottomSuit.innerHTML = newSuit;
 
-  //cambiar color
-
-  if (newSuit == "&hearts;" || newSuit == "&diams;") {
-    newTopSuit.style.color = "red";
-    newBottomSuit.style.color = "red";
+  let newNumber = document.querySelector(".number");
+  newNumber.innerHTML = randomNumber();
+  //color rojo en class card
+  let card = document.querySelector(".card");
+  if (newSuit === "&hearts;" || newSuit === "&diams;") {
+    card.style.color = "red";
+  } else {
+    card.style.color = "black";
   }
-});
+
+  //función botón
+  document.querySelector("#btn").addEventListener("click", cardRandom);
+};
